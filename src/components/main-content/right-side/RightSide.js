4c9af0d02e5e6f1662cce8a4cb477f6d91ef93f5
Septiana Yoga <septianayoga30@gmail.com>
2022-05-19 13:56:50 +0700

    adding functionality to add to the local storage

--- src/components/main-content/right-side/RightSide.js
+++ src/components/main-content/right-side/RightSide.js
@@ -2,17 +2,17 @@ import React, { useState } from "react";
 import { cartWhite } from "../../../assets";
 import "./RightSide.css";
 
+const localStorageData = (data) => {
+  if (typeof Storage !== "undefined") {
+    localStorage.setItem("productTotal", data);
+    alert(data + " product has been added to the cart!");
+  } else {
+    alert("Storage is not available, the apps will not work as it should be");
+  }
+};
+
 const RightSide = () => {
   const [totalBuy, setTotalBuy] = useState(0);
-
-  const buyHandle = (action) => {
-    if (action === "+") {
-      setTotalBuy(totalBuy + 1);
-    } else if (action === "-") {
-      setTotalBuy(totalBuy - 1);
-    }
-  };
-
   return (
     <div className="right-container">
       <h3 className="company-name">SNEAKER COMPANY</h3>
@@ -29,11 +29,24 @@ const RightSide = () => {
       <span className="product-price">$250.00</span>
       <div className="counter-cart-container">
         <div className="counter">
-          <button className="minus-button">-</button>
-          <span className="product-counter">0</span>
-          <button className="plus-button">+</button>
+          <button
+            onClick={() => (totalBuy <= 0 ? "" : setTotalBuy(totalBuy - 1))}
+            className="minus-button"
+          >
+            -
+          </button>
+          <span className="product-counter">{totalBuy}</span>
+          <button
+            onClick={() => setTotalBuy(totalBuy + 1)}
+            className="plus-button"
+          >
+            +
+          </button>
         </div>
-        <button className="add-to-cart">
+        <button
+          onClick={() => localStorageData(totalBuy)}
+          className="add-to-cart"
+        >
           <img src={cartWhite} alt="cart" />
           &nbsp; Add to cart
         </button>
